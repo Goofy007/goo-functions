8bf4cc805186b1ff6597e38f3a12f08e478b2cbc
许丹琦 <1204253728@qq.com>
2020-05-25 11:30:42 +0800

    forEachObject

--- function-playground/play.js
+++ function-playground/play.js
@@ -3,3 +3,9 @@ var fc  = require("../lib/es6-functional.js")
 var array = [1,2,3]
 
 fc.forEach(array,(data) => console.log(data));
+
+var object = {a:1,b:2}
+
+fc.forEachObject(object,(k,v) => {
+  console.log(k+":"+v)
+})
